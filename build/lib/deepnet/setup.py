b9688d4ec50a8019c8f510b424d4ca016c5097dd
kcydesu <$occerOBY1998>
2019-11-12 18:17:05 -0500

    Small bug fixes and adding pkg_resources

---
+++ build/lib/deepnet/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='deepnet',
+    version='1.0.0',
+    packages=['deepnet'],
+    url='',
+    license='',
+    author='Casey Bruno',
+    author_email='cabruno98@gmail.com',
+    description='Class using AlexNet to classify images',
+    install_requires=['torch','torch-vision'],
+    package_data={'data': ['*.txt']}
+)
